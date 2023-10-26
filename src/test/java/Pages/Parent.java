aac2f35d42c1077f33e65a18f8e515277c7be880
Enes <enes.buke12@gmail.com>
2023-10-26 22:53:33 +0300

    Project Update

--- src/test/java/Pages/Parent.java
+++ src/test/java/Pages/Parent.java
@@ -12,29 +12,34 @@ import org.testng.Assert;
 import java.time.Duration;
 
 public class Parent {
-    public WebDriverWait wait=new WebDriverWait(BaseDriver.getDriver(), Duration.ofSeconds(15));
+    public WebDriverWait wait = new WebDriverWait(BaseDriver.getDriver(), Duration.ofSeconds(15));
+
     public void scrollToElement(WebElement element) {
         wait.until(ExpectedConditions.visibilityOf(element));
         JavascriptExecutor js = (JavascriptExecutor) BaseDriver.getDriver();
         js.executeScript("arguments[0].scrollIntoView();", element);
     }
-    public void clickMethod(WebElement elm){
+
+    public void clickMethod(WebElement elm) {
         wait.until(ExpectedConditions.visibilityOf(elm));
         elm.click();
     }
-    public void sendKeysMethod(WebElement elm,String keys){
+
+    public void sendKeysMethod(WebElement elm, String keys) {
         wait.until(ExpectedConditions.visibilityOf(elm));
         scrollToElement(elm);
         elm.click();
         elm.clear();
         elm.sendKeys(keys);
     }
-    public void myDisplayedAssert(WebElement elm){
+
+    public void myDisplayedAssert(WebElement elm) {
         wait.until(ExpectedConditions.visibilityOf(elm));
         Assert.assertTrue(elm.isDisplayed());
     }
-    public void verifyContainsText(WebElement element, String value){
-        wait.until(ExpectedConditions.textToBePresentInElement(element,value));
+
+    public void verifyContainsText(WebElement element, String value) {
+        wait.until(ExpectedConditions.textToBePresentInElement(element, value));
         Assert.assertTrue(element.getText().toLowerCase().contains(value.toLowerCase()));
 
         new Actions(BaseDriver.getDriver()).sendKeys(Keys.ESCAPE).build().perform();
